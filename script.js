f2845d81a114316962b0cbd08f26d31f905c804b
flavia ribeiro <flavinasribs@gmail..com>
2025-10-20 10:56:39 -0300

    Feature 2: Botao Limpar

--- script.js
+++ script.js
@@ -12,10 +12,17 @@ window.document.addEventListener("DOMContentLoaded", function(){
         document.querySelector("#result").innerHTML = inputText.toLowerCase()
 
     })
+    // Botão primeira Letra Minúscula
     window.document.querySelector("#btnPrimeiraLetraMaiuscula").addEventListener("click", function(){
         let inputText = document.querySelector("#input-text").value
         let modifiedText = inputText.charAt(0).toUpperCase() + inputText.slice(1).toLowerCase();
         document.querySelector("#result").innerHTML = modifiedText;
 
     })
+    // Botão Limpar
+    window.document.querySelector("#limpar-button").addEventListener("click", function() {
+        document.querySelector("#input-text").value = "";
+        document.querySelector("#result").innerHTML = "";
+    });
+
 })
